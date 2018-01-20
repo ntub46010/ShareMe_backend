5d25af4b167560fea5f3503fcf2f8e7de44ec989
Vincent <eric61011@gmail.com>
2018-01-20 18:51:01 +0800

    Initial commit

---
+++ src/org/shareme/Constant.java
@@ -0,0 +1,53 @@
+package org.shareme;
+
+public class Constant {
+	public static final String DB_IP = "localhost";
+	public static final String DB_NAME = "ShareMe";
+	public static final String DB_USER = "sa";
+	public static final String DB_PASSWORD = "2rrxuolx";
+	public static final String DB_CONNECT_STRING = String.format(
+			"jdbc:sqlserver://%s;databaseName=%s;user=%s;password=%s", DB_IP, DB_NAME, DB_USER, DB_PASSWORD
+			);
+	
+	public static final String KEY_STATUS = "Status";
+	public static final String KEY_USER_ID = "UserId";
+	public static final String KEY_PASSWORD = "Password";
+	public static final String KEY_NAME = "Name";
+	public static final String KEY_DEPARTMENT = "Department";
+	public static final String KEY_GENDER = "Gender";
+	public static final String KEY_EMAIL = "Email";
+	public static final String KEY_TYPE = "Type";
+	public static final String KEY_KEYWORD = "Keyword";
+	public static final String KEY_TITLE = "Title";
+	public static final String KEY_PRICE = "Price";
+	public static final String KEY_CONDITION = "Condition";
+	public static final String KEY_NOTE = "Note";
+	public static final String KEY_PS = "PS";
+	public static final String KEY_SELLER = "Seller";
+	public static final String KEY_PHOTO1 = "Photo1";
+	public static final String KEY_PHOTO2 = "Photo2";
+	public static final String KEY_PHOTO3 = "Photo3";
+	public static final String KEY_PHOTO4 = "Photo4";
+	public static final String KEY_PHOTO5 = "Photo5";
+	public static final String KEY_PRODUCT = "Product";
+	public static final String KEY_PRODUCT_ID = "ProductId";
+	public static final String KEY_ANYWAY = "Anyway";
+	public static final String KEY_FAVORITE = "Favorite";
+	public static final String KEY_IS_ADD = "IsAdd";
+	public static final String KEY_MEMBER_ID = "MemberId";
+	public static final String KEY_IS_SETTING = "IsSetting";
+	public static final String KEY_GIVER_ID = "GiverId";
+	public static final String KEY_RECEIVER_ID = "ReceiverId";
+	public static final String KEY_VALUE = "Value";
+	public static final String KEY_SENDER_ID = "SenderId";
+	public static final String KEY_AVATAR = "Avatar";
+	
+	
+	public static final String KEY_PROFILE = "Profile";
+	public static final String KEY_PRODUCTS = "Products";
+	public static final String KEY_STOCK = "Stock";
+	public static final String KEY_MAILS = "Mails";
+	public static final String KEY_MESSAGE = "Message";
+	
+
+}
---
+++ src/org/shareme/WebService.java
@@ -0,0 +1,510 @@
+package org.shareme;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.microsoft.sqlserver.jdbc.SQLServerException;
+
+@Path("/")
+public class WebService {
+	//public static void main(String[] args) {}
+	
+	//註冊
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("/register")
+	public Response register (String reqJSONString) throws JSONException {
+		JSONObject optObj = new JSONObject();
+		try {
+			JSONObject iptObj = new JSONObject(reqJSONString);
+			String userId = iptObj.getString(Constant.KEY_USER_ID);
+			String pwd = iptObj.getString(Constant.KEY_PASSWORD);
+			String name = iptObj.getString(Constant.KEY_NAME);
+			String dep = iptObj.getString(Constant.KEY_DEPARTMENT);
+			String gender = iptObj.getString(Constant.KEY_GENDER);
+			String email = iptObj.getString(Constant.KEY_EMAIL);
+			executeUpdate(
+					String.format("exec 註冊 '%s', '%s', '%s', '%s', '%s', '%s'", userId, pwd, name, dep, gender, email
+			));
+			optObj.put(Constant.KEY_STATUS, true);
+		} catch (JSONException|SQLException e) {
+			optObj.put(Constant.KEY_STATUS, false);
+		}
+		return getJSONResponse(optObj.toString());
+	}
+
+	//登入
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("/login")
+	public Response login (String reqJSONString) throws JSONException {
+		JSONObject optObj = new JSONObject();
+		String userId = "", pwd = "";
+		try {
+			JSONObject iptObj = new JSONObject(reqJSONString);
+			userId = iptObj.getString(Constant.KEY_USER_ID);
+			pwd = iptObj.getString(Constant.KEY_PASSWORD);
+			
+			optObj.put(Constant.KEY_PROFILE, new JSONObject(getQueryJSONString(
+					String.format("exec 登入 '%s', '%s'", userId, pwd), false
+			)));
+			optObj.put(Constant.KEY_STATUS, true);
+		} catch (JSONException|SQLException e) {
+			optObj.put(Constant.KEY_STATUS, false);
+		}
+		return getJSONResponse(optObj.toString());
+	}
+
+	//列示商品
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("/list_products")
+	public Response listProducts (String reqJSONString) throws JSONException {
+		JSONObject optObj = new JSONObject();
+		try {
+			JSONObject iptObj = new JSONObject(reqJSONString);
+			String type = iptObj.getString(Constant.KEY_TYPE);
+			String keyword = iptObj.getString(Constant.KEY_KEYWORD);
+			
+			optObj.put(Constant.KEY_PRODUCTS, new JSONArray(getQueryJSONString(
+					String.format("exec 列示商品 '%s', '%s'", type, keyword)
+					, true)));
+			
+			//避免未找到商品也會加入true
+			if (optObj.get(Constant.KEY_PRODUCTS).toString().equals("[]"))
+				throw new SQLException();
+			
+			optObj.put(Constant.KEY_STATUS, true);
+		} catch (JSONException|SQLException e) {
+			optObj.put(Constant.KEY_STATUS, false);
+		}
+		return getJSONResponse(optObj.toString());
+	}
+	
+	//刊登商品
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("/post_product")
+	public Response postProduct (String reqJSONString) throws JSONException {
+		JSONObject optObj = new JSONObject();
+		try {
+			JSONObject iptObj = new JSONObject(reqJSONString);
+			String title = iptObj.getString(Constant.KEY_TITLE);
+			String price = iptObj.getString(Constant.KEY_PRICE);
+			String type = iptObj.getString(Constant.KEY_TYPE);
+			String condition = iptObj.getString(Constant.KEY_CONDITION);
+			String note = iptObj.getString(Constant.KEY_NOTE);
+			String ps = iptObj.getString(Constant.KEY_PS);
+			String seller = iptObj.getString(Constant.KEY_SELLER);
+			String photo1 = iptObj.getString(Constant.KEY_PHOTO1);
+			String photo2 = iptObj.getString(Constant.KEY_PHOTO2);
+			String photo3 = iptObj.getString(Constant.KEY_PHOTO3);
+			String photo4 = iptObj.getString(Constant.KEY_PHOTO4);
+			String photo5 = iptObj.getString(Constant.KEY_PHOTO5);
+						
+			optObj.put(Constant.KEY_PRODUCT, new JSONObject(getQueryJSONString(
+					String.format("exec 刊登商品 '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s'", 
+							title, price, type, condition, note, ps, seller, photo1, photo2, photo3, photo4, photo5)
+					, false)));
+			optObj.put(Constant.KEY_STATUS, true);
+		} catch (JSONException|SQLException e) {
+			e.printStackTrace();
+			optObj.put(Constant.KEY_STATUS, false);
+		}
+		return getJSONResponse(optObj.toString());
+	}
+	
+	//顯示商品詳情
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("/product_detail")
+	public Response showProduct (String reqJSONString) throws JSONException {
+		JSONObject optObj = new JSONObject();
+		try {
+			JSONObject iptObj = new JSONObject(reqJSONString);
+			String userId = iptObj.getString(Constant.KEY_USER_ID);
+			String productId = iptObj.getString(Constant.KEY_PRODUCT_ID);
+			String anyway = iptObj.getString(Constant.KEY_ANYWAY);
+			
+			JSONObject obj = new JSONObject(getQueryJSONString(
+					String.format("exec 顯示商品詳情 '%s', '%s', '%s'", userId, productId, anyway), false
+			)); //沒找到商品會發失利外傳回false
+			//修正最愛的值為布林
+			if (obj.getString(Constant.KEY_FAVORITE).equals("1"))
+				obj.put(Constant.KEY_FAVORITE, true);
+			else
+				obj.put(Constant.KEY_FAVORITE, false);
+			
+			optObj.put(Constant.KEY_PRODUCT, obj);
+			optObj.put(Constant.KEY_STATUS, true);
+		} catch (JSONException|SQLException e) {
+			//e.printStackTrace();
+			optObj.put(Constant.KEY_STATUS, false);
+		}
+		return getJSONResponse(optObj.toString());
+	}
+	
+	//加入我的最愛
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("/add_favorite")
+	public Response addFavorite (String reqJSONString) throws JSONException {
+		JSONObject optObj = new JSONObject();
+		try {
+			JSONObject iptObj = new JSONObject(reqJSONString);
+			String userId = iptObj.getString(Constant.KEY_USER_ID);
+			String productId = iptObj.getString(Constant.KEY_PRODUCT_ID);
+			optObj = new JSONObject(getQueryJSONString(					
+					String.format("exec 加入我的最愛 '%s', '%s'", userId, productId), false
+			));
+			if (optObj.get(Constant.KEY_IS_ADD).equals("1"))
+				optObj.put(Constant.KEY_IS_ADD, true);
+			else
+				optObj.put(Constant.KEY_IS_ADD, false);				
+			
+			optObj.put(Constant.KEY_STATUS, true);
+		} catch (JSONException|SQLException e) {
+			e.printStackTrace();
+			optObj.put(Constant.KEY_STATUS, false);
+		}
+		return getJSONResponse(optObj.toString());
+	}
+	
+	//顯示會員檔案
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("/show_profile")
+	public Response showProfile (String reqJSONString) throws JSONException {
+		JSONObject optObj = new JSONObject();
+		try {
+			JSONObject iptObj = new JSONObject(reqJSONString);
+			String userId = iptObj.getString(Constant.KEY_USER_ID);
+			String memberId = iptObj.getString(Constant.KEY_MEMBER_ID);
+			boolean isSetting = iptObj.getBoolean(Constant.KEY_IS_SETTING);
+			
+			JSONObject obj = new JSONObject(getQueryJSONString(
+					String.format("exec 顯示會員檔案 '%s', '%s'", userId, memberId), false
+			));
+			//若不是在設定頁面，則將密碼移除
+			if (!isSetting)
+				obj.remove(Constant.KEY_PASSWORD);
+			
+			optObj.put(Constant.KEY_PROFILE, obj);
+			
+			//避免未找到會員也會加入true
+			if (optObj.get(Constant.KEY_PROFILE).toString().equals("[]"))
+				throw new SQLException();
+			else {
+				optObj.put(Constant.KEY_STOCK, new JSONArray(getQueryJSONString(
+						String.format("exec 列示庫存 '%s'", userId), true					
+				)));				
+			}			
+			optObj.put(Constant.KEY_STATUS, true);
+		} catch (JSONException|SQLException e) {
+			e.printStackTrace();
+			optObj.put(Constant.KEY_STATUS, false);
+		}
+		return getJSONResponse(optObj.toString());
+	}	
+	
+	//評價會員
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("/evaluate")
+	public Response evaluate (String reqJSONString) throws JSONException {
+		JSONObject optObj = new JSONObject();
+		try {
+			JSONObject iptObj = new JSONObject(reqJSONString);
+			String giverId = iptObj.getString(Constant.KEY_GIVER_ID);
+			String receiverId = iptObj.getString(Constant.KEY_RECEIVER_ID);
+			String value = iptObj.getString(Constant.KEY_VALUE);
+			executeUpdate(
+					String.format("exec 評價會員 '%s', '%s', '%s'", giverId, receiverId, value)
+			);
+			optObj.put(Constant.KEY_STATUS, true);
+		} catch (JSONException|SQLException e) {
+			optObj.put(Constant.KEY_STATUS, false);
+		}
+		return getJSONResponse(optObj.toString());
+	}
+	
+	//列示我的最愛
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("/list_favorite")
+	public Response listFavorite (String reqJSONString) throws JSONException {
+		JSONObject optObj = new JSONObject();
+		try {
+			JSONObject iptObj = new JSONObject(reqJSONString);
+			String userId = iptObj.getString(Constant.KEY_USER_ID);
+			optObj.put(Constant.KEY_FAVORITE, new JSONArray(getQueryJSONString(
+					String.format("exec 列示我的最愛 '%s'", userId), true
+			)));
+			optObj.put(Constant.KEY_STATUS, true);
+		} catch (JSONException|SQLException e) {
+			optObj.put(Constant.KEY_STATUS, false);
+		}
+		return getJSONResponse(optObj.toString());
+	}
+	
+	//列示信箱訊息
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("/list_mails")
+	public Response listMails (String reqJSONString) throws JSONException {
+		JSONObject optObj = new JSONObject();
+		try {
+			JSONObject iptObj = new JSONObject(reqJSONString);
+			String userId = iptObj.getString(Constant.KEY_USER_ID);
+			optObj.put(Constant.KEY_MAILS, new JSONArray(getQueryJSONString(
+					String.format("exec 列示信箱訊息 '%s'", userId), true
+			)));
+			optObj.put(Constant.KEY_STATUS, true);
+		} catch (JSONException|SQLException e) {
+			optObj.put(Constant.KEY_STATUS, false);
+		}
+		return getJSONResponse(optObj.toString());
+	}
+	
+	//列示交談訊息
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("/list_message")
+	public Response listMessage (String reqJSONString) throws JSONException {
+		JSONObject optObj = new JSONObject();
+		try {
+			JSONObject iptObj = new JSONObject(reqJSONString);
+			String userId = iptObj.getString(Constant.KEY_USER_ID);
+			String memberId = iptObj.getString(Constant.KEY_MEMBER_ID);
+			String productId = iptObj.getString(Constant.KEY_PRODUCT_ID);
+			optObj = new JSONObject(getQueryJSONString(
+					String.format("exec 搜尋大頭貼", memberId), false
+			));
+			optObj.put(Constant.KEY_MESSAGE, new JSONArray(getQueryJSONString(
+					String.format("exec 列示交談訊息 '%s', '%s', '%s'", userId, memberId, productId), true
+			)));
+			
+			optObj.put(Constant.KEY_STATUS, true);
+		} catch (JSONException|SQLException e) {
+			optObj.put(Constant.KEY_STATUS, false);
+		}
+		return getJSONResponse(optObj.toString());
+	}
+	
+	//傳送訊息
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("/send_message")
+	public Response sendMessage (String reqJSONString) throws JSONException {
+		JSONObject optObj = new JSONObject();
+		try {
+			JSONObject iptObj = new JSONObject(reqJSONString);
+			String senderId = iptObj.getString(Constant.KEY_SENDER_ID);
+			String receiverId = iptObj.getString(Constant.KEY_RECEIVER_ID);
+			String productId = iptObj.getString(Constant.KEY_PRODUCT_ID);
+			String message = iptObj.getString(Constant.KEY_MESSAGE);
+			executeUpdate(
+					String.format("exec 傳送訊息 '%s', '%s', '%s', '%s'", 
+									senderId, receiverId, productId, message)					
+			);			
+			optObj.put(Constant.KEY_STATUS, true);
+		} catch (JSONException|SQLException e) {
+			optObj.put(Constant.KEY_STATUS, false);
+		}
+		return getJSONResponse(optObj.toString());
+	}
+	
+	//列示庫存
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("/show_stock")
+	public Response showStock (String reqJSONString) throws JSONException {
+		JSONObject optObj = new JSONObject();
+		try {
+			JSONObject iptObj = new JSONObject(reqJSONString);
+			String userId = iptObj.getString(Constant.KEY_USER_ID);
+			optObj.put(Constant.KEY_STOCK, new JSONArray(getQueryJSONString(
+					String.format("exec 列示庫存 '%s'", userId), true					
+			)));
+			//避免未找到商品也會加入true
+			if (optObj.get(Constant.KEY_STOCK).toString().equals("[]"))
+				throw new SQLException();
+			
+			optObj.put(Constant.KEY_STATUS, true);
+		} catch (JSONException|SQLException e) {
+			optObj.put(Constant.KEY_STATUS, false);
+		}
+		return getJSONResponse(optObj.toString());
+	}
+	
+	//編輯商品
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("/edit_product")
+	public Response editProduct (String reqJSONString) throws JSONException {
+		JSONObject optObj = new JSONObject();
+		try {
+			JSONObject iptObj = new JSONObject(reqJSONString);
+			String productId = iptObj.getString(Constant.KEY_PRODUCT_ID);
+			String title = iptObj.getString(Constant.KEY_TITLE);
+			String price = iptObj.getString(Constant.KEY_PRICE);
+			String type = iptObj.getString(Constant.KEY_TYPE);
+			String condition = iptObj.getString(Constant.KEY_CONDITION);
+			String note = iptObj.getString(Constant.KEY_NOTE);
+			String ps = iptObj.getString(Constant.KEY_PS);
+			String photo1 = iptObj.getString(Constant.KEY_PHOTO1);
+			String photo2 = iptObj.getString(Constant.KEY_PHOTO2);
+			String photo3 = iptObj.getString(Constant.KEY_PHOTO3);
+			String photo4 = iptObj.getString(Constant.KEY_PHOTO4);
+			String photo5 = iptObj.getString(Constant.KEY_PHOTO5);
+			
+			executeUpdate(
+					String.format("exec 編輯商品 '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s', '%s'", 
+									productId, title, price, type, condition, note, ps, photo1, photo2, photo3, photo4, photo5)
+			);
+			optObj.put(Constant.KEY_STATUS, true);
+		} catch (JSONException|SQLException e) {
+			optObj.put(Constant.KEY_STATUS, false);
+		}
+		return getJSONResponse(optObj.toString());
+	}
+	
+	//下架商品
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("/drop_product")
+	public Response dropProduct (String reqJSONString) throws JSONException {
+		JSONObject optObj = new JSONObject();
+		try {
+			JSONObject iptObj = new JSONObject(reqJSONString);
+			String productId = iptObj.getString(Constant.KEY_PRODUCT_ID);
+			executeUpdate(
+					String.format("exec 下架商品 '%s'", productId)
+			);
+			optObj.put(Constant.KEY_STATUS, true);
+		} catch (JSONException|SQLException e) {
+			optObj.put(Constant.KEY_STATUS, false);
+		}
+		return getJSONResponse(optObj.toString());
+	}
+	
+	//編輯個人檔案
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("/edit_profile")
+	public Response editProfile (String reqJSONString) throws JSONException {
+		JSONObject optObj = new JSONObject();
+		try {
+			JSONObject iptObj = new JSONObject(reqJSONString);
+			String userId = iptObj.getString(Constant.KEY_USER_ID);
+			String avatar = iptObj.getString(Constant.KEY_AVATAR);
+			String name = iptObj.getString(Constant.KEY_NAME);
+			String department = iptObj.getString(Constant.KEY_DEPARTMENT);
+			String email = iptObj.getString(Constant.KEY_EMAIL);
+			String pwd = iptObj.getString(Constant.KEY_PASSWORD);
+			executeUpdate(
+					String.format("exec 編輯個人檔案 '%s', '%s', '%s', '%s', '%s', '%s'", 
+									userId, avatar, name, department, email, pwd)
+			);
+			optObj.put(Constant.KEY_STATUS, true);
+		} catch (JSONException|SQLException e) {
+			optObj.put(Constant.KEY_STATUS, false);
+		}
+		return getJSONResponse(optObj.toString());
+	}
+		
+	private String getQueryJSONString (String mandate, boolean convertToArray) throws SQLException, SQLServerException, JSONException {
+		System.out.println(mandate);
+		
+    	Connection con = null;
+    	Statement stmt = null;
+    	ResultSet rs = null;
+    	JSONArray optAry = new JSONArray(); //預設[]
+    	try {
+    		Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+    		con = DriverManager.getConnection(Constant.DB_CONNECT_STRING);
+    		stmt = con.createStatement();
+    		rs = stmt.executeQuery(mandate);
+    		
+    		ResultSetMetaData rsmd = rs.getMetaData();
+    		int colCount = rsmd.getColumnCount();
+			while (rs.next()) {
+				JSONObject tmpObj = new JSONObject();
+    			for (int i=1; i<=colCount; i++) {					
+    				tmpObj.put(rsmd.getColumnName(i), rs.getString(i));					
+    			}
+    			optAry.put(tmpObj);        			
+    		}   		
+    	} catch (ClassNotFoundException e) {
+    	} finally {
+    		try {
+        		if (rs != null) {
+            		rs.close();		
+            		rs = null;	
+        		}
+        		if (stmt != null) {
+            		stmt.close();
+            		stmt = null;    			
+        		}
+        		if (con != null) {
+            		con.close();
+            	    con = null;
+        		}
+    		} catch (SQLException e) {
+        	}
+    	}
+    	
+    	if (convertToArray) //若發生例外，傳回[]、{}
+    		return optAry.toString();
+    	else {
+    		JSONObject optObj = optAry.getJSONObject(0);
+    		return optObj.toString();
+    	}
+	}
+    	
+	private void executeUpdate (String mandate) throws SQLException {
+		System.out.println(mandate);
+		
+		Connection con = null;
+    	Statement stmt = null;
+    	try {
+    		Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");    		
+    		con = DriverManager.getConnection(Constant.DB_CONNECT_STRING);
+    		stmt = con.createStatement();
+    		stmt.executeUpdate(mandate);
+    		
+    	} catch (ClassNotFoundException e) {
+    	} finally {
+    		try {
+        		if (stmt != null) {
+            		stmt.close();
+            		stmt = null;    			
+        		}
+        		if (con != null) {
+            		con.close();			              
+            	    con = null;    			
+        		}	
+    		} catch (SQLException e) {
+        	}
+    	}
+	}
+	
+    private Response getJSONResponse(String resJSONString) {
+    	return Response.ok(resJSONString, MediaType.APPLICATION_JSON).build();
+    }
+
+}
